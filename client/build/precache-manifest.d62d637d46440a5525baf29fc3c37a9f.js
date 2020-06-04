6ba56e8774a9fb2f3805ccf36c8fcd8d410f5a8c
Dishebh Bhayana <dishebh27@gmail.com>
2020-06-04 17:44:35 +0530

    updating client build

---
+++ client/build/precache-manifest.d62d637d46440a5525baf29fc3c37a9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "410745257a9d0eead258a6197b4bb2fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87c39342953e10435333",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "11d3152ce10a64d675eb",
+    "url": "/static/js/2.1cf3430b.chunk.js"
+  },
+  {
+    "revision": "e293c143e177a32373554d6ca4c08bba",
+    "url": "/static/js/2.1cf3430b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87c39342953e10435333",
+    "url": "/static/js/main.373e587a.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
